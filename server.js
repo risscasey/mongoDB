61fe690c9c34e5b0a335c22794ca45587770abce
Larissa Godfrey <932400452@bun2-206-15.local>
2019-08-21 11:54:26 +1200

    adding ID param

--- server.js
+++ server.js
@@ -13,9 +13,25 @@ app.get('/', function(req, res){
     res.send('Welcome to our Products API. Use endpoints to filter out the data');
 });
 
+app.get('/products', function(req, res){
+    res.send(allProducts);
+});
 
+app.get('/id=:idNo', function(req, res){
+  const idNoParam = req.params.idNo;
+  console.log(idNoParam);
+
+  // let requestedID = [];
+  // for (var i = 0; i < allProducts.length; i++) {
+  //   if (idNoParam == allProducts[i].id) {
+  //     requestedID.push(allProducts[i]);
+  //   } else {
+  //     console.log('Sorry that was not a valid ID');
+  //   }
+  // }
+  // res.send(requestedID);
+});
 
 app.listen(port, () => {
-    console.clear();
     console.log(`application is running on port ${port}`)
 });
