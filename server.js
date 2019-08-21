21c08e6ef9da2016d4969a789289eb58bb9f5e46
Larissa Godfrey <932400452@bun2-206-15.local>
2019-08-22 11:50:58 +1200

    Created Post request

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const allProducts = require('./data/products');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
 
 app.use(function(req, res, next){
     console.log(`${req.method} request for ${req.url}`);
@@ -19,36 +26,51 @@ app.get('/products', function(req, res){
 
 app.get('/products/id=:idNo', function(req, res){
   const IDParam = req.params.idNo;
-      let filteredData = [];
-      for (var i = 0; i < allProducts.length; i++) {
-          if(allProducts[i].id.toString() === IDParam){
-              filteredData.push(allProducts[i]);
-          }
-      }
-      res.send(filteredData);
+  let filteredData = [];
+  for (var i = 0; i < allProducts.length; i++) {
+    if(allProducts[i].id.toString() === IDParam){
+      filteredData.push(allProducts[i]);
+    }
+  }
+  res.send(filteredData);
 });
 
 app.get('/products/edit/id=:idNo', function(req, res){
   const IDParam = req.params.idNo;
-      let filteredData = [];
-      for (var i = 0; i < allProducts.length; i++) {
-          if(allProducts[i].id.toString() === IDParam){
-              filteredData.push(allProducts[i]);
-          }
-      }
-      res.send(filteredData);
+  let filteredData = [];
+  for (var i = 0; i < allProducts.length; i++) {
+    if(allProducts[i].id.toString() === IDParam){
+        filteredData.push(allProducts[i]);
+    }
+  }
+  res.send(filteredData);
 });
 
 app.get('/products/delete/id=:idNo', function(req, res){
   const IDParam = req.params.idNo;
-      let filteredData = [];
-      for (var i = 0; i < allProducts.length; i++) {
-          if(allProducts[i].id.toString() === IDParam){
-              filteredData.push(allProducts[i]);
-          }
-      }
-      res.send(filteredData);
+  let filteredData = [];
+  for (var i = 0; i < allProducts.length; i++) {
+    if(allProducts[i].id.toString() === IDParam){
+        filteredData.push(allProducts[i]);
+    }
+  }
+  res.send(filteredData);
 });
+
+
+
+app.post('/products', function(req, res){
+  // console.log('post req made');
+  // console.log(req.body);
+  let product = {
+    name: req.body.name,
+    price: req.body.price,
+    message: 'about to sent this to DB'
+  }
+  res.send(product);
+})
+
+
 
 app.listen(port, () => {
     console.log(`application is running on port ${port}`)
