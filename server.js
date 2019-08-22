e7747c82caa8469a219b991c65e013eca3c4cf09
Larissa Godfrey <932400452@bun2-206-15.local>
2019-08-23 11:56:32 +1200

    completed post req

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+const config = require('./config.json');
+
+mongoose.connect(`mongodb+srv://RissCasey:${config.MONGO_PASSWORD}@firstcluster-89ezs.mongodb.net/shop?retryWrites=true&w=majority`, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we on mongoose boiz');
+});
 
 const allProducts = require('./data/products');
 
@@ -57,20 +67,43 @@ app.get('/products/delete/id=:idNo', function(req, res){
   res.send(filteredData);
 });
 
-
+const Product = require('./models/products');
 
 app.post('/products', function(req, res){
   // console.log('post req made');
   // console.log(req.body);
-  let product = {
+  // let product = {
+  //   name: req.body.name,
+  //   price: req.body.price,
+  //   message: 'about to sent this to DB'
+  // }
+  // res.send(product);
+
+  const product = new Product ({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price,
-    message: 'about to sent this to DB'
-  }
-  res.send(product);
-})
+    price: req.body.price
+  });
+  console.log(product);
 
+  product.save().then(result => {
+    res.send(result);
+  }).catch(err => res.send(err));
+});
 
+app.post('/contact', function(req, res){
+  const user = new Product ({
+    _id: new mongoose.Types.ObjectId(),
+    fName: req.body.fName,
+    lName: req.body.lName,
+    email: req.body.email,
+  });
+  console.log(user);
+
+  user.save().then(result => {
+    res.send(result);
+  }).catch(err => res.send(err));
+});
 
 app.listen(port, () => {
     console.log(`application is running on port ${port}`)
